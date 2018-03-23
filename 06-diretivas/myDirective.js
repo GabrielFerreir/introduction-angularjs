57750c56d8e64c6f57e55d17a05620355ee1e431
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-03-23 09:14:00 -0300

    Alterando elementos com diretivas

--- 06-diretivas/myDirective.js
+++ 06-diretivas/myDirective.js
@@ -1,17 +1,26 @@
+// angular.module('meuApp').directive('myDirective', function () {
+//   return {
+//     restrict: 'E',
+//     templateUrl: 'myDirective.html',
+//     scope: {
+//       text: '=',
+//       myFunc: '='
+//     },
+//     link: function (scope, elem, attrs) {
+//       var vm = scope;
+//       vm.onChange = function () {
+//         console.log('Function from component', vm.text);
+//         vm.myFunc(vm.text);
+//       }
+//     }
+//   }
+// });
 angular.module('meuApp').directive('myDirective', function () {
   return {
-    restrict: 'E',
-    templateUrl: 'myDirective.html',
-    scope: {
-      text: '=',
-      myFunc: '='
-    },
+    restrict: 'A',
     link: function (scope, elem, attrs) {
       var vm = scope;
-      vm.onChange = function () {
-        console.log('Function from component', vm.text);
-        vm.myFunc(vm.text);
-      }
+      elem[0].style.backgroundColor = 'red';
     }
   }
 });
